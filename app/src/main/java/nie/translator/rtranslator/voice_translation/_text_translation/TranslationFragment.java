6145b199f3a42950d8803f7bde4c0ace4ddc8d7a
Jyoti negi <jn600079@gmail.com>
2026-05-07 21:53:13 +0530

    Added final Text Translation mode

--- app/src/main/java/nie/translator/rtranslator/voice_translation/_text_translation/TranslationFragment.java
+++ app/src/main/java/nie/translator/rtranslator/voice_translation/_text_translation/TranslationFragment.java
@@ -1,4 +1,5 @@
-// Text Translation mode
+// Text Translation mode.
+// Allow user to translate text from one language to another.
 package nie.translator.rtranslator.voice_translation._text_translation;
 
 import static android.content.Context.CLIPBOARD_SERVICE;
